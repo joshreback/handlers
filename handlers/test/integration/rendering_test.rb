b669598bdb23a37d46290a6528a9af5a0a707f89
Josh Reback <joshr@edmodo.com>
2015-09-07 01:04:35 -0400

    all specs pass

---
+++ handlers/test/integration/rendering_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class RenderingTest < ActionDispatch::IntegrationTest
+  test ".rb template handler" do
+    get "/handlers/rb_handler"
+    expected = "This is my first <b>template handler</b>!"
+    assert_match expected, response.body
+  end
+
+  test ".string template handler" do
+    get "/handlers/string_handler"
+    expected = "Congratulations! You just created another template handler!"
+    assert_match expected, response.body
+  end
+end
