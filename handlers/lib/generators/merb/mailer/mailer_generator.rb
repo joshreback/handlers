7109d365544c0ade981c6a395b28692e76cf4669
Josh Reback <joshr@edmodo.com>
2015-09-08 03:58:01 -0400

    hacked current version of generator to align with books implementaiton

--- handlers/lib/generators/merb/mailer/mailer_generator.rb
+++ handlers/lib/generators/merb/mailer/mailer_generator.rb
@@ -5,12 +5,33 @@ module Merb
   module Generators
     class MailerGenerator < Erb::Generators::MailerGenerator
       source_root File.expand_path("../templates", __FILE__)
-      binding.pry
+
+      def copy_view_files
+        view_base_path = File.join("app/views", class_path, file_name + '_mailer')
+        empty_directory view_base_path
+
+        actions.each do |action|
+          @action = action
+
+          if formats.empty?
+            @path = File.join(view_base_path, filename_with_extensions(action, format))
+            template filename_with_extensions(:view, format), @path
+          else
+            formats.each do |format|
+              @path = File.join(view_base_path, filename_with_extensions(action, format))
+              template filename_with_extensions(:view, format), @path
+            end
+          end
+        end
+      end
 
       protected
+      def formats
+        [] # Our templates have no format
+      end
+
       def format
-        binding.pry
-        nil # Our templates have no format
+        nil
       end
 
       def handler
