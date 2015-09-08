26202a723002fc294f29abb785d9b3100ce4ae61
Josh Reback <joshr@edmodo.com>
2015-09-08 03:26:18 -0400

    working on generator

--- handlers/lib/handlers.rb
+++ handlers/lib/handlers.rb
@@ -3,7 +3,7 @@ require 'rdiscount'
 require 'pry'
 
 module Handlers
-  module MERB
+  module Merb
     def self.erb_handler
       @@erb_handler ||= ActionView::Template.registered_template_handler(:erb)
     end
@@ -24,4 +24,4 @@ ActionView::Template.register_template_handler :string,
   lambda { |template| "%Q{#{template.source}}" }
 ActionView::Template.register_template_handler :md,
   lambda { |template| "RDiscount.new(#{template.source.inspect}).to_html" }
-ActionView::Template.register_template_handler :merb, Handlers::MERB
+ActionView::Template.register_template_handler :merb, Handlers::Merb
